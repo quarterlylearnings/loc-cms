d75fcbcb722b673d681eda594df54f036764d9fd
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-04-21 16:05:32 -0400

    Adds pointer cursor, crucial

--- app/components/AudioUploader.tsx
+++ app/components/AudioUploader.tsx
@@ -1,7 +1,8 @@
 "use client";
 export default function AudioUploader() {
-
-    const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {}
+    const handleUpload = async (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {};
     return (
         <div className="p-4 max-w-lg bg-white rounded-lg shadow">
             <h1 className="text-xl text-center text-gray-700">
@@ -11,7 +12,17 @@ export default function AudioUploader() {
                 Upload an audio file of a reading to be transcribed
             </p>
             <div className="mt-4 flex justify-center">
-                <input type="file" accept="audio/*" onChange={handleUpload} />
+                <input
+                    type="file"
+                    accept="audio/*"
+                    onChange={handleUpload}
+                    className="block w-full text-sm text-gray-500
+                  file:mr-4 file:py-2 file:px-4
+                  file:rounded-full file:border-0
+                  file:text-sm file:font-semibold
+                  file:bg-violet-50 file:text-violet-700
+                  file:cursor-pointer hover:file:bg-violet-100"
+                />
             </div>
         </div>
     );
